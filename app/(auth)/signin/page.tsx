b7a9d95048169ae6d100db0aff1d03159fe2a0b8
Paula Lima <paula.lima@mvfglobal.com>
2023-05-05 18:03:50 +0100

    Create AuthForm Pages and Components

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,3 +1,5 @@
-export default function SigninPage() {
-    return <div>signin</div>
+import AuthForm from "@/components/AuthForm";
+
+export default function Signin() {
+  return <AuthForm mode="signin" />;
 }
