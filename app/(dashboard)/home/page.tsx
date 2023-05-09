236bb816bcc5ebbbe5b2f89b991375093eadf0bd
Paula Lima <paula.lima@mvfglobal.com>
2023-05-09 18:15:23 +0100

    Create Greetings component

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -12,7 +12,9 @@ export default async function Page() {
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
-            {/** greetings here */}
+            <Suspense fallback={<GreetingsSkeleton />}>
+                <Greetings />
+            </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
